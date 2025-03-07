36ab0fa77815807c539091e9f32364a1458a393e
rennyhyde <rennyhyde@protonmail.com>
2025-03-07 12:45:13 -0500

    Motif-based MIDI generation

---
+++ M8.1-Samoa-2009/gen_MIDI_motifs.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+import datetime
+from midiutil.MidiFile import MIDIFile
+import itertools
+
+from parse import * 
+df = parse_txt("./20090929_M8.1_Samoa_EQ_PKD_LFE_pos.txt")
+
+# Scale times to target length
+original_time = total_time(df)
+target_time = 3 # minutes
+time_scaling = (target_time*60)/original_time.total_seconds()
+df['playback-time'] = df['playback-time']*time_scaling
+# display(df)
+
+# Melody generation
+scale = [0, 2, 4, 5, 7, 9, 11]
+note_durs = [4, 8, 16, 32]
+BPM = 120
+whole = (60/120)*4
+combs = []
+for i in range(len(scale)+1):
+    els = [list(x) for x in itertools.combinations_with_replacement(scale, i)]
+    # els = [list(x) for x in itertools.combinations(scale, i)] #Use this for combinations without repetition
+    combs.extend(els)
+
+# print(len(combs))
+class motif:
+    import random
+    notes = []
+    timing = []
+    ID = 0
+    def __init__(self, index):
+        self.notes = combs[index * 12]
+        for i in range(len(self.notes)):
+            self.timing.append(note_durs[int(self.random.random()*len(note_durs))])
+            if self.random.random() > 0.8:
+                if self.random.random() > 0.5:
+                    self.notes[i] = self.notes[i]+12
+                else:
+                    self.notes[i] = self.notes[i]-12
+        self.ID = index
+    def play(self, mf, time, rel_time):
+        #MyMIDI.addNote(track, channel, pitch, time, duration, volume)
+        for i in range(len(self.notes)):
+            mf.addNote(self.ID-1, 1, self.notes[i]+60, (time - time%(whole/16))*(whole/4), (whole / self.timing[i])*(whole/4), 127)
+            time = time + whole / self.timing[i]
+            # print("MIDI play")
+
+
+# Generate MIDI
+mf = MIDIFile(numTracks=88, removeDuplicates=False, deinterleave=False)
+
+motifs = []
+for i in range(88):
+    motifs.append(motif(i))
+for index, row in df.iterrows():
+    motifs[row['along-strike-id(1-88)']].play(mf, row['playback-time'].total_seconds(), 0)    
+
+# print(mf)
+with open("output.mid", 'wb') as outf:
+    mf.writeFile(outf)
--- M8.1-Samoa-2009/parse.py
+++ M8.1-Samoa-2009/parse.py
@@ -3,37 +3,50 @@ Data parsing for tremor data from: https://earthquake.usgs.gov/earthquakes/event
 
 
 """
-import pandas as pd
-import numpy as np
-from IPython.display import display
-import datetime
+def parse_txt(path):
+    import pandas as pd
+    import numpy as np
+    from IPython.display import display
+    import datetime
 
-# Read in file, create pandas dataframe
-infile = open("./20090929_M8.1_Samoa_EQ_PKD_LFE_pos.txt")
-lines = infile.readlines()
-data = []
-for i in range(len(lines)):
-    if (len(lines[i].split()) < 18):        # Some of the lines at the end of the file are 16 items long -> figure out why
-        continue                            # For now, ignore these lines
-    data.append(lines[i].split())
-df = pd.DataFrame(data, columns = ['along-strike-id(1-88)', 'along-strike-distance_km', 'year', 'month', 'day', 's_of_day', 'hr', 'min', 'sec', 'ccsum', 'meancc', 'med_cc', 'seqday', 'ID', 'latitude', 'longitude', 'depth', 'n_chan'])
-# display(df)
+    # Read in file, create pandas dataframe
+    infile = open(path)
+    lines = infile.readlines()
+    data = []
+    for i in range(len(lines)):
+        if (len(lines[i].split()) < 18):        # Some of the lines at the end of the file are 16 items long -> figure out why
+            continue                            # For now, ignore these lines
+        data.append(lines[i].split())
+    df = pd.DataFrame(data, columns = ['along-strike-id(1-88)', 'along-strike-distance_km', 'year', 'month', 'day', 's_of_day', 'hr', 'min', 'sec', 'ccsum', 'meancc', 'med_cc', 'seqday', 'ID', 'latitude', 'longitude', 'depth', 'n_chan'])
+    # display(df)
 
-# Calculate relative time
-# All of these tremors occur on the same day, so we can neglect the year, month, and day for now
-abs_times = []
-rel_times = []
-ref = datetime.datetime(2009, 9, 29, 17, 48, 10)
+    # Calculate relative time
+    # All of these tremors occur on the same day, so we can neglect the year, month, and day for now
+    abs_times = []
+    rel_times = []
+    playback_times = []
+    rel_i = 0
+    ref = datetime.datetime(2009, 9, 29, 17, 48, 10)
 
-for series_name, series in df.items():
-    if series_name == 'ID':
-        continue
-    df[series_name] = pd.to_numeric(df[series_name])
+    for series_name, series in df.items():
+        if series_name == 'ID':
+            continue
+        df[series_name] = pd.to_numeric(df[series_name])
 
-for index, row in df.iterrows():
-    abs = datetime.datetime(row['year'], row['month'], row['day'], row['hr'], row['min'], int(row['sec']), int((row['sec']*100)%100)*10000)
-    abs_times.append(abs)
-    rel_times.append(abs - ref)
-df['absolute time'] = abs_times
-df['relative time'] = rel_times
-display(df)
+
+    for index, row in df.iterrows():
+        abs = datetime.datetime(row['year'], row['month'], row['day'], row['hr'], row['min'], int(row['sec']), int((row['sec']*100)%100)*10000)
+        abs_times.append(abs)
+        if (index == 0):
+            rel_i = abs - ref
+        rel_times.append(abs - ref)
+        playback_times.append(abs - ref - rel_i)
+    df['absolute-time'] = abs_times
+    df['relative-time'] = rel_times
+    df['playback-time'] = playback_times
+    return df
+
+def total_time(df):
+    import pandas as pd
+    import datetime
+    return df.max(axis=0)['relative-time'] - df.min(axis=0)['relative-time']
